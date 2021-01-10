ecaaf2a48253db1e0165e784cc28c286e7821c42
s01va <jinn0525@gmail.com>
2021-01-10 15:40:14 +0900

    03 분수찾기

--- step_by/08/03_1193.py
+++ step_by/08/03_1193.py
@@ -1,2 +1,12 @@
 X = int(input())
-print(str((X+1)//2) + "/" + str(X%3))
+N = 1
+M = 1
+while True:
+	if M >= X:
+		break
+	N += 1
+	M = M + N
+if N % 2 == 1:
+	print(str(M-X+1) + "/" + str(N-M+X))
+else:
+	print(str(N-M+X) + "/" + str(M-X+1))
