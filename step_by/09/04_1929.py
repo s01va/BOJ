ba27db150914a82c074daa2f8af31e2819647b21
s01va <jinn0525@gmail.com>
2021-01-14 23:48:04 +0900

    소수찾기

--- step_by/09/04_1929.py
+++ step_by/09/04_1929.py
@@ -1,11 +1,11 @@
-import sys
+import sys, math
 M, N = map(int, sys.stdin.readline().rstrip().split())
 for num in range(M,N+1):
 	f = True
 	if num == 1:
 		f = False
 		continue
-	for n in range(2, num):
+	for n in range(2, int(math.sqrt(num))+1):
 		if num % n == 0:
 			f = False
 			break
