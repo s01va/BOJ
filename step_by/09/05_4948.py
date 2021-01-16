e3c0e560d9d6b3cf2024f98b5f7f765d8d4090e6
s01va <jinn0525@gmail.com>
2021-01-17 00:27:23 +0900

    베르트랑 공준

--- step_by/09/05_4948.py
+++ step_by/09/05_4948.py
@@ -1,18 +1,20 @@
 import math
+
+def sieve(num):
+	if num != 1:
+		for i in range(2, int(math.sqrt(num)) + 1):
+			if num % i == 0:
+				return False
+	else:
+		return False
+	return True
+
 while(True):
 	n = int(input())
 	dn = 0
 	if n == 0:
 		break
 	for m in range(n+1, 2*n+1):
-		f = True
-		if m == 1:
-			f = False
-			continue
-		for i in range(2, int(math.sqrt(m))+1):
-			if m % i == 0:
-				f = False
-				break
-		if f == True:
+		if sieve(m) == True:
 			dn += 1
 	print(dn)
